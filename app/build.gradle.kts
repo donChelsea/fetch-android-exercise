5d3c03c61a9b0e1f3c5f46ed9eab6b3d23444462
Chelsea Katsidzira <1133280@iheartmedia.com>
2025-02-05 12:24:50 -0500

    set up initial dependencies for dagger, retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -19,9 +21,13 @@ android {
     }
 
     buildTypes {
+        debug {
+            buildConfigField("String", "BASE_URL", "\"https://fetch-hiring.s3.amazonaws.com/mobile.html/\"")
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            buildConfigField("String", "BASE_URL", "\"https://fetch-hiring.s3.amazonaws.com/mobile.html/\"")
         }
     }
     compileOptions {
@@ -33,6 +39,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
@@ -53,4 +60,22 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Compose lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.0")
+
+    // Dagger/Hilt
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-compiler:2.51.1")
+
+    // Retrofit + Gson
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // Timber
+    implementation("com.jakewharton.timber:timber:5.0.1")
+}
+
+kapt {
+    correctErrorTypes = true
 }
---
+++ app/src/main/java/com/example/fetch_android_exercise/FetchAndroidExerciseApp.kt
@@ -0,0 +1,7 @@
+package com.example.fetch_android_exercise
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FetchAndroidExerciseApp: Application()
